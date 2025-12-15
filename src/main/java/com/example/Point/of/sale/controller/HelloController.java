3c0427104b546654e95f88d8b2f226d8e7bc74fa
ahmed-elhamri <elhamri949@gmail.com>
2025-12-15 01:11:10 +0100

    New Changes ...

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from spring boot application :)";
+        return "New Hello from spring boot application :)";
     }
 
     @GetMapping("/user")
